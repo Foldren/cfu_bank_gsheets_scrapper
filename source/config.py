be341191db32079cd7e8228fbe56ecb606a0ed97
Foldren <kir.dem.98@ya.ru>
2023-12-29 18:19:58 +0300

    fix envs

--- source/config.py
+++ source/config.py
@@ -1,12 +1,12 @@
-from os import getenv
-from environs import Env
+from os import getenv, environ
 from pathlib import Path
+from dotenv import load_dotenv
 
-env = Env()
-env.read_env('.env')
+load_dotenv()
+
 
 IS_THIS_LOCAL = "Pycharm" in str(Path.cwd())
-MYSQL_URL = getenv('MYSQL_URL') if IS_THIS_LOCAL else env("MYSQL_URL")
+MYSQL_URL = environ['MYSQL_URL'] if IS_THIS_LOCAL else getenv("MYSQL_URL")
 
 # Интервал перезапуска, каждые 24 часа
 LOOP_INTERVAL_TIME = 86400
@@ -16,8 +16,6 @@ BANKS_RUS_NAMES = {
     'module': 'Модуль',
     'tochka': 'Точка',
 }
-SECRET_KEY = env('SECRET_KEY')
-PROXY6NET_PROXIES = {
-    "socks5://": getenv("PROXY_HTTPS_URL") if IS_THIS_LOCAL else env('PROXY_HTTPS_URL'),
-}
+SECRET_KEY = getenv('SECRET_KEY')
+PROXY6NET_PROXIES = {"socks5://": getenv('PROXY_HTTPS_URL')}
 
